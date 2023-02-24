adb3def54584d1d36e7a25056955ba2749144099
Marnix Prins <marnix.prins2@klm.com>
2023-02-24 15:22:09 +0100

    bugfix in simplex algorithm

--- main.py
+++ main.py
@@ -9,21 +9,18 @@ mip.mode = 1
 # Set variables
 mip.add_decision_variable(lb=0,ub=10,name='x_1')
 mip.add_decision_variable(lb=0,ub=10,name='x_2')
-# mip.add_decision_variable(lb=0,ub=10,name='x_3')
+mip.add_decision_variable(lb=0,ub=10,name='x_3')
 
 # Set objective function
-objective_function = '7 * x_1 /+ 10 * x_2'
+objective_function = '8 * x_1 /+ 10 * x_2 /+ 7 * x_3'
 mip.set_objective_function(objective_function)
 
 # Set constraints
-constraint_string = '-1 * x_1/+ 3 * x_2 <= 6'
+constraint_string = '1 * x_1/+ 3 * x_2/ + 2 * x_3 <= 10'
 mip.set_constraint(constraint_string)
-constraint_string = '7 * x_1/+1 * x_2 <= 35'
+constraint_string = '1 * x_1/+5 * x_2/ + 1 * x_1 <= 8'
 mip.set_constraint(constraint_string)
-# constraint_string = '0 * x_1/+1 * x_2 <= 3' /+0 * x_3 <= 3'
-# mip.set_constraint(constraint_string)
-# constraint_string = '0 * x_1/+0* x_2/+1 * x_3 <= 2'
-# mip.set_constraint(constraint_string)
+
 
 mip.update()
 # Solve mip
